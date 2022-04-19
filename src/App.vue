43442d25393d2e259aef0b8b4d200dbb970ac80d
huangxuezhi <huangxuezhi@wakedata.com>
2022-04-19 17:08:10 +0800

    init vue3 project

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld
+    msg="Hello Vue 3 + TypeScript + Vite"
+    v-model:count="count"
+    v-model:userInfo="useInfo"
+  />
+</template>
+
+<script lang="ts">
+import { ref, reactive } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const count = ref(0);
+    const useInfo = reactive({
+      name: "KK",
+      age: 2,
+    });
+
+    return {
+      useInfo,
+      count,
+    };
+  },
+};
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1 v-bind="$attrs" ref="h1">{{ msg }}</h1>
+
+  <button type="button" @click="addCount">count is: {{ count }}</button>
+  <div>{{ userInfo.name }}</div>
+</template>
+
+<script  lang="ts">
+export default {
+  props: ["count", "userInfo", "msg"],
+  // emits: ["update:count"],
+  setup(props, {}) {},
+  methods: {
+    addCount() {
+      this.$emit("update:count", this.count + 1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
